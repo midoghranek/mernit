94ba8533a8ab478675e0475fd0dd5de2ef12bc99
midoghranek <midoghranek@hotmail.com>
2019-10-06 20:43:18 +0200

    build

---
+++ build/precache-manifest.497a198aaa03712bcb97b3fe729cf79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a701d3926e2484850ab440c577172aec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7835024a134359576e48",
+    "url": "/static/css/main.e0738ce9.chunk.css"
+  },
+  {
+    "revision": "a5b626247cabc2a2ebc0",
+    "url": "/static/js/2.9e0ba5ba.chunk.js"
+  },
+  {
+    "revision": "7835024a134359576e48",
+    "url": "/static/js/main.68e3c373.chunk.js"
+  },
+  {
+    "revision": "d6d5090de39e1242a4b8",
+    "url": "/static/js/runtime-main.ab8e1cf3.js"
+  }
+]);
